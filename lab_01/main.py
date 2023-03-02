79d3f195fe921a802ba9e001e83e91456432b0fd
Buldakov Mikhail <mishabul2003@gmail.com>
2023-03-02 18:48:40 +0300

    done + no_tests

--- lab_01/main.py
+++ lab_01/main.py
@@ -2,7 +2,7 @@ import math as m
 
 import matplotlib.pyplot as plt
 
-def read_table():
+def read_table_with_der():
     table = []
     line = input()
     while line != '~':
@@ -29,6 +29,32 @@ def read_table():
     return table
 
 
+def read_table():
+    table = []
+    line = input()
+    while line != '~':
+        line = line.split(' ')
+        if len(line) == 2:
+            x, y = line
+        else:
+            return None
+
+        if not (x and y):
+            return None
+        try:
+            x = float(x)
+            y = float(y)
+        except:
+            return None
+
+        table.append([x, y, 1])
+        line = input()
+
+    table.sort(key=lambda x: x[0])
+    return table
+
+
+
 def divided_diff(table):
     temp = [[0] * (len(table)) for i in range(len(table) + 2)]
     for i in range(len(table)):
@@ -52,10 +78,10 @@ def select_points_newton(table, x, n):
     new_table = []
     
     pos = 0
-    while table[pos][0] < x:
+    while pos < len(table) - 1 and table[pos][0] < x:
         pos += 1
     
-    if abs(table[pos][0] - x) < abs(table[pos - 1][0] - x):
+    if pos != 0 and abs(table[pos][0] - x) > abs(table[pos - 1][0] - x):
         pos -= 1
     
     
@@ -84,14 +110,15 @@ def select_points_hermite(table, x, n):
     new_table = []
     
     pos = 0
-    while table[pos][0] < x:
+    while pos < len(table) - 1 and table[pos][0] < x:
         pos += 1
     
-    if abs(table[pos][0] - x) > abs(table[pos - 1][0] - x):
+    if pos != 0 and abs(table[pos][0] - x) > abs(table[pos - 1][0] - x):
         pos -= 1
     
     new_table.append(table[pos].copy())
-    new_table.append(table[pos].copy())       
+    if len(new_table) < n + 1:
+        new_table.append(table[pos].copy())       
     
     l_bound = pos - 1
     r_bound = pos + 1
@@ -169,55 +196,105 @@ def root_find_hermite(table, n):
     return approximate_hermite(inverse_table, 0, n)
 
 
+def system_solution(table_1, table_2, n):
+    res_table = []
+    
+    for i in table_2:
+        res_table.append([i[0], approximate_newton(table_1, i[0], n) - i[1], i[2]])
+        
+    i = 0
+    while i < len(res_table) - 1:
+        if res_table[i + 1][1] > res_table[i][1]:
+            res_table.pop(i)
+        else:
+            i += 1
+    
+    #print(*res_table, sep='\n')
+    
+    return root_find_newton(res_table, n)
+
+
 def main():
-    #table = [[-0.5, 0.707, 0], [-0.25, 0.924, 0], [0, 1, 0], [0.25, 0.924, 0], [0.5, 0.707, 0], [0.75, 0.383, 0], [1, 0, 0]]
+    
     print("Введите x, y, y` через пробел, для окончания ввода введите '~': ")
-    table = read_table()
+    table = read_table_with_der()
     
-    # plt.plot([x[0] for x in table], [y[1] for y in table])
-    # plt.show()
-    print(table)
-    
-    print(approximate_hermite(table, 0.5, 7))
-    print(approximate_newton(table, 0.5, 7))
-    
-    print(root_find_newton(table, 6))
-    print(root_find_hermite(table, 6))
-    
-    
-    # if not table:
-    #     print("Ошибка ввода")
-    #     return
+    if not table:
+        print("Ошибка ввода")
+        return
 
     # n = input('Введите n: ')
     # if not n.isdigit():
     #     print("Ошибка ввода")
     #     return
     # n = int(n)
-
-    # x = input('Введите х: ')
-    # try:
-    #     x = float(x)
-    # except:
-    #     print('Ошибка ввода')
-    #     return
     
-
-
-    # x = input('Введите х: ')
-    # try:
-    #     x = float(x)
-    # except:
-    #     print('Ошибка ввода')
+    # if len(table) < n + 1:
+    #     print("Недостаточно точек")
     #     return
+
+    x = input('Введите х: ')
+    try:
+        x = float(x)
+    except:
+        print('Ошибка ввода')
+        return
     
-    # x = input('Введите х: ')
-    # try:
-    #     x = float(x)
-    # except:
-    #     print('Ошибка ввода')
-    #     return
-
+    if x < min([x[0] for x in table]) or x > max([x[0] for x in table]):
+        print("Возникла экстраполяция") 
+    
+    
+    print("Приблизительное значение:")
+    print('-' * 64)  
+    print('|{:^20}|{:^20}|{:^20}|'.format('n', 'Newton', 'Hermite'))
+    print('-' * 64)    
+    for i in range(len(table)):
+        print("|{:^20d}|{:^20.3f}|{:^20.3f}|".format(i, approximate_newton(table, x, i), approximate_hermite(table, x, i)))
+    print('-' * 64)  
+    
+    
+    print("Нахождение корня:")
+    print('-' * 64)  
+    print('|{:^20}|{:^20}|{:^20}|'.format('n', 'Newton', 'Hermite'))
+    print('-' * 64)    
+    for i in range(len(table)):
+        print("|{:^20d}|{:^20.3f}|{:^20.3f}|".format(i, root_find_newton(table, i), root_find_hermite(table, i)))
+    print('-' * 64)  
+    
+    
+    print("Введите таблицу для первой функции. x, y через пробел, для окончания ввода введите '~': ")
+    table_1 = read_table()
+    
+    if not table_1:
+        print("Ошибка ввода")
+        return
+    
+    print("Введите таблицу для второй функции. x, y через пробел, для окончания ввода введите '~': ")
+    table_2 = read_table()
+    
+    if not table_2:
+        print("Ошибка ввода")
+        return
+    
+    print("Решение системы:")
+    print('-' * 43)  
+    print('|{:^20}|{:^20}|'.format('n', 'Newton'))
+    print('-' * 43)    
+    for i in range(8):
+        print("|{:^20d}|{:^20.3f}|".format(i, system_solution(table_1, table_2, i)))
+    print('-' * 43)  
+    
+    fig, ax = plt.subplots()
+    ax.plot([x[0] for x in table], [x[1] for x in table])
+    ax.grid()
+    
+    fig_2, ax_2 = plt.subplots()
+    ax_2.plot([x[0] for x in table_1], [x[1] for x in table_1])
+    ax_2.plot([x[0] for x in table_2], [x[1] for x in table_2])
+    ax_2.grid()
+    
+    plt.show()
+    
 
 if __name__ == '__main__':
     main()
