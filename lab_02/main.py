09fc207b8a8cbd3cddf06d2e4fbcf308374e25bd
Buldakov Mikhail <mishabul2003@gmail.com>
2023-03-09 22:14:35 +0300

    Error which was found by VOLN have been fixed

--- lab_02/main.py
+++ lab_02/main.py
@@ -31,7 +31,7 @@ if __name__ == '__main__':
     divided_diffs_right = divided_diff(selected_points_right)
     
     newton_sec_dir_left = divided_diffs_left[1] + divided_diffs_left[2] * (4 * min(x) - 2 * selected_points_left[2][0])
-    newton_sec_dir_right = divided_diffs_right[1] + divided_diffs_right[2] * (4 * min(x) - 2 * selected_points_right[2][0])
+    newton_sec_dir_right = divided_diffs_right[1] + divided_diffs_right[2] * (4 * max(x) - 2 * selected_points_right[2][0])
     
     spline_natural = Spline(table, 0, 0)
     spline_left_edge_newton = Spline(table, newton_sec_dir_left, 0)
